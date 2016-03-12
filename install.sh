2f34708fcecfa68232ba0c09fcda21cec6db8f0b
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-03-12 00:58:52 -0500

    Fixed installing in current directory.

--- install.sh
+++ install.sh
@@ -11,7 +11,11 @@ rm install.sh
 
 # Download the online judge into the current directory
 echo "Downloading latest version of online judge."
-git clone https://github.com/LisgarComputingClub/Online-Judge.git .
+git clone https://github.com/LisgarComputingClub/Online-Judge.git
+
+# Move judge to current directory
+echo "Moving into current directory"
+mv Online-Judge $PWD
 
 # Install judge dependencies
 echo "Installing judge dependencies"
