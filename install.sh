666959add8b7dc30d3bdf0283e1ee96c17ea4697
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-03-12 01:05:21 -0500

    Just fixed it AAAAAAAAAAAAAAAAAAAAAAAAA

--- install.sh
+++ install.sh
@@ -2,28 +2,23 @@
 echo "Attempting to install Git and Node.js"
 sudo apt-get install git nodejs -y
 
+# Go into upper directory
+cd ../
+
 # Delete unnecessary files
-echo "Deleting unnecessary files"
-rm README.md
-rm LICENSE
-# Goodbye cruel world
-rm install.sh
+echo "Deleting unnecessary install files"
+rm Online-Judge-Install -rf
 
 # Download the online judge into the current directory
 echo "Downloading latest version of online judge."
 git clone https://github.com/LisgarComputingClub/Online-Judge.git
 
-# Move judge to current directory
-echo "Moving into current directory"
-mv Online-Judge $PWD
+# Move into judge directory
+cd Online-Judge
 
 # Install judge dependencies
 echo "Installing judge dependencies"
 npm install express ejs mongodb body-parser
-
-# Rename current directory
-echo "Renaming current directory"
-mv $PWD ${PWD%/*}/Online-Judge
 
 # Output a friendly message
 echo "Done installing online judge."
