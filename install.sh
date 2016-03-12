6141b9b34f0bafaff27464b09da9de515118ff2c
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-03-12 01:19:07 -0500

    Added on the fly (untested) dependency fetching.

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
 # Install Git, Node.js and NPM
 echo "Attempting to install Git and Node.js"
 sudo apt-get install git nodejs -y
+
+# Get temporary install files
+wget https://gist.githubusercontent.com/Porso7/1dea6140143961a3c762/raw/133d8b39976784f110c3bb99124b0abcbe38a924/dependencies.sh
 
 # Go into upper directory
 cd ../
@@ -18,7 +22,7 @@ cd Online-Judge
 
 # Install judge dependencies
 echo "Installing judge dependencies."
-npm install express ejs mongodb body-parser
+bash Online-Judge-Install/dependencies.sh
 
 # Output a friendly message
 echo "Done installing online judge."
