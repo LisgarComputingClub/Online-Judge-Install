d58781f5d6ccdb3a7b9f4b78666d47d5b2cc0f1e
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-03-12 00:16:42 -0500

    Added install flag to skip prompts.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Install Git, Node.js and NPM
-sudo apt-get install git nodejs npm
+sudo apt-get install git npm nodejs -y
 
 # Download the online judge
 git clone https://github.com/LisgarComputingClub/Online-Judge.git
