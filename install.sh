5bb751e6639f928bd4430f23b70452d89f10edf1
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-03-12 00:46:27 -0500

    Added more friendly messages.

--- install.sh
+++ install.sh
@@ -1,19 +1,24 @@
 # Install Git, Node.js and NPM
+echo "Attempting to install Git and Node.js"
 sudo apt-get install git nodejs -y
 
 # Delete unnecessary files
+echo "Deleting unnecessary files"
 rm README.md
 rm LICENSE
 # Goodbye cruel world
 rm install.sh
 
 # Download the online judge into the current directory
+echo "Downloading latest version of online judge."
 git clone https://github.com/LisgarComputingClub/Online-Judge.git .
 
 # Install judge dependencies
+echo "Installing judge dependencies"
 npm install express ejs mongodb body-parser
 
 # Rename current directory
+echo "Renaming current directory"
 mv $PWD ${PWD%/*}/Online-Judge
 
 # Output a friendly message
