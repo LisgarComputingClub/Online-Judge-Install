59d47c896b72c40192492fd8f4396785d4ab214f
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-03-12 00:43:06 -0500

    Made judge install in current directory.

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 # Install Git, Node.js and NPM
 sudo apt-get install git nodejs -y
 
-# Download the online judge
-git clone https://github.com/LisgarComputingClub/Online-Judge.git
-
-# Go into the online judge directory and install dependencies
-cd Online-Judge
-npm install express ejs
-
 # Delete unnecessary files
-cd ../
 rm README.md
 rm LICENSE
+# Goodbye cruel world
 rm install.sh
 
-# Goodbye creuwl world
+# Download the online judge into the current directory
+git clone https://github.com/LisgarComputingClub/Online-Judge.git .
+
+# Install judge dependencies
+npm install express ejs mongodb body-parser
+
+# Rename current directory
+mv $PWD ${PWD%/*}/Online-Judge
+
+# Output a friendly message
+echo "Done installing online judge."
