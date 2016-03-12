cd432d9030f6f8eeb49b769a8b739ffc677e86bd
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-03-12 00:30:25 -0500

    Install dependencies and remove files.

--- install.sh
+++ install.sh
@@ -3,3 +3,15 @@ sudo apt-get install git nodejs -y
 
 # Download the online judge
 git clone https://github.com/LisgarComputingClub/Online-Judge.git
+
+# Go into the online judge directory and install dependencies
+cd Online-Judge
+npm install express ejs
+
+# Delete unnecessary files
+cd ../
+rm README.md
+rm LICENSE
+rm install.sh
+
+# Goodbye creuwl world
