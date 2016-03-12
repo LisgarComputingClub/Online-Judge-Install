31755cb6cc1ae3a9bbe76fcbef3a7576a859b1a8
Ivan Fonseca <ivanfonseca55@gmail.com>
2016-03-12 01:31:04 -0500

    Fixed references to online judge install files.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # downloads the latest version of the online judge from Github.
 
 # Install Git, Node.js and NPM
-echo "Attempting to install Git and Node.js"
+echo "Attempting to install Git and Node.js..."
 sudo apt-get install git nodejs -y
 
 # Get temporary install files
@@ -14,19 +14,19 @@ wget https://gist.githubusercontent.com/Porso7/1dea6140143961a3c762/raw/133d8b39
 cd ../
 
 # Download the online judge into the current directory
-echo "Downloading latest version of online judge."
+echo "Downloading latest version of online judge..."
 git clone https://github.com/LisgarComputingClub/Online-Judge.git
 
 # Move into judge directory
 cd Online-Judge
 
 # Install judge dependencies
-echo "Installing judge dependencies."
-bash Online-Judge-Install/dependencies.sh
+echo "Installing judge dependencies..."
+bash ../Online-Judge-Install/dependencies.sh
 
 # Delete unnecessary files
-echo "Deleting unnecessary install files."
-rm Online-Judge-Install -rf
+echo "Cleaning up..."
+rm ../Online-Judge-Install -rf
 
 # Output a friendly message
 echo "Done installing online judge."
